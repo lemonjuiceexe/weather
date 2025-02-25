731aea15f852660edbb5e0171d80f4e11484773a
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2025-02-25 19:57:24 +0100

    Add MUI X graph for temperature

--- src/components/WeatherGraph.jsx
+++ src/components/WeatherGraph.jsx
@@ -1,9 +1,55 @@
+import { LineChart } from "@mui/x-charts";
 import styles from './WeatherGraph.module.css';
+
+const DUMMY_TEMPERATURES = [
+    { date: "2021-10-01", weekday: "Fri", temperature: 20 },
+    { date: "2021-10-02", weekday: "Sat", temperature: 22 },
+    { date: "2021-10-03", weekday: "Sun", temperature: 21 },
+    { date: "2021-10-04", weekday: "Mon", temperature: 19 },
+    { date: "2021-10-05", weekday: "Tue", temperature: 18 },
+    { date: "2021-10-06", weekday: "Wed", temperature: 17 },
+    { date: "2021-10-07", weekday: "Thu", temperature: 20 },
+];
 
 export default function WeatherGraph() {
     return (
         <div className={styles.wrapper}>
             <p>hi</p>
+            <LineChart
+                dataset={DUMMY_TEMPERATURES}
+                yAxis={[{
+                    type: "number",
+                    dataKey: "temperature",
+                    min: 15,
+                    max: 25
+                }]}
+                series={[{
+                    type: "line",
+                    dataKey: "temperature",
+                    color: "white",
+                }]}
+                slots={{
+                    mark: MarkElement
+                }}
+                sx={{
+                    '& .MuiLineElement-root': {
+                        strokeWidth: 1,
+                    },
+                    ".MuiChartsAxis-line": {
+                        stroke: "white",
+                    },
+                    ".MuiChartsAxis-tick": {
+                        stroke: "white",
+                    },
+                    ".MuiChartsAxis-tickLabel": {
+                        fill: "white",
+                    },
+                }}
+            />
         </div>
     );
+}
+
+function MarkElement({x, y, color}) {
+    return (<circle cx={x} cy={y} r={2} fill={color} />);
 }
